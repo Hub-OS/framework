7df118e006e7d2dc0280a286bd82a1442b3eddf2
Arthur Cosentino <arthurcose@gmail.com>
2023-02-16 17:59:14 -0500

    Implement Future for AsyncTask

--- src/async_task/task.rs
+++ src/async_task/task.rs
@@ -1,3 +1,7 @@
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
 #[must_use = "tasks are cancelled when dropped, use `.detach()` to run the task in the background"]
 pub struct AsyncTask<T> {
     task: async_task::Task<T>,
@@ -24,5 +28,14 @@ impl<T> AsyncTask<T> {
     /// Run the task without requiring the result to be used
     pub fn detach(self) {
         self.task.detach()
+    }
+}
+
+impl<T> Future for AsyncTask<T> {
+    type Output = T;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        use futures_lite::future::FutureExt;
+        self.task.poll(cx)
     }
 }
