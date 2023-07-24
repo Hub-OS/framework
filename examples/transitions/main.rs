1d98fe8bf5779484030d24c900861ab624dd968b
Arthur Cosentino <arthurcose@gmail.com>
2023-07-24 02:59:30 -0400

    Add transition example

---
+++ examples/transitions/main.rs
@@ -0,0 +1,115 @@
+use framework::logging::*;
+use framework::prelude::*;
+
+mod push_transition;
+use push_transition::*;
+
+fn main() -> anyhow::Result<()> {
+    default_logger::init!();
+
+    let game = Game::new("Transitions", (800, 600));
+
+    game.run(|game_io| ExampleScene::new(game_io, 0))
+}
+
+// transition
+
+// example scene
+
+struct ExampleScene {
+    camera: OrthoCamera,
+    sprite: Sprite,
+    depth: usize,
+    focused: bool,
+    next_scene: NextScene,
+}
+
+impl ExampleScene {
+    fn new(game_io: &mut GameIO, depth: usize) -> Self {
+        let mut camera = OrthoCamera::new(game_io, Vec2::new(800.0, 600.0));
+        camera.invert_y(true);
+
+        let texture_bytes = match depth {
+            0 => &include_bytes!("A.png")[..],
+            1 => &include_bytes!("B.png")[..],
+            _ => &include_bytes!("C.png")[..],
+        };
+
+        let texture = Texture::load_from_memory(game_io, texture_bytes).unwrap();
+
+        let mut sprite = Sprite::new(game_io, texture);
+        sprite.set_origin(sprite.size() * 0.5);
+        sprite.set_scale(Vec2::ONE * 10.0);
+
+        Self {
+            camera,
+            sprite,
+            depth,
+            focused: true,
+            next_scene: NextScene::None,
+        }
+    }
+}
+
+impl Scene for ExampleScene {
+    fn next_scene(&mut self) -> &mut NextScene {
+        &mut self.next_scene
+    }
+
+    fn enter(&mut self, _: &mut GameIO) {
+        self.focused = true;
+    }
+
+    fn update(&mut self, game_io: &mut GameIO) {
+        if !self.focused {
+            // block setting next_scene when we're not in focus
+            return;
+        }
+
+        let just_pressed_space = game_io.input().was_key_just_pressed(Key::Space);
+        let just_pressed_shift = game_io.input().was_key_just_pressed(Key::LShift);
+
+        // handle new scene creation
+        if self.depth < 2 && just_pressed_space {
+            let direction = match self.depth {
+                0 => TransitionDirection::Left,
+                1 => TransitionDirection::Up,
+                _ => unreachable!(),
+            };
+
+            let scene = ExampleScene::new(game_io, self.depth + 1);
+            let transition = PushTransition::new(game_io, direction, Duration::from_secs_f32(0.5));
+
+            self.next_scene = NextScene::new_push(scene).with_transition(transition);
+        }
+
+        // handle returning to a previous scene
+        if self.depth > 0 && self.next_scene.is_none() && just_pressed_shift {
+            let direction = match self.depth {
+                0 => unreachable!(),
+                1 => TransitionDirection::Right,
+                _ => TransitionDirection::Down,
+            };
+
+            let transition = PushTransition::new(game_io, direction, Duration::from_secs_f32(0.5));
+            self.next_scene = NextScene::new_pop().with_transition(transition);
+        }
+
+        if self.next_scene.is_some() {
+            // we've set the next scene, so we're no longer in focus
+            self.focused = false;
+        }
+    }
+
+    fn draw(&mut self, game_io: &mut GameIO, render_pass: &mut RenderPass) {
+        self.camera.scale_to_window(game_io.window());
+
+        let uniforms = [self.camera.as_binding()];
+        let mut render_queue =
+            SpriteQueue::new_with_default_pipeline(game_io, uniforms).with_inverted_y(true);
+
+        render_queue.draw_sprite(&self.sprite);
+
+        render_pass.consume_queue(render_queue);
+    }
+}
---
+++ examples/transitions/push_transition.rs
@@ -0,0 +1,114 @@
+use framework::prelude::*;
+
+#[warn(dead_code)]
+pub enum TransitionDirection {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl TransitionDirection {
+    fn chebyshev_vector(&self) -> Vec2 {
+        match self {
+            TransitionDirection::Up => Vec2::new(0.0, -1.0),
+            TransitionDirection::Down => Vec2::new(0.0, 1.0),
+            TransitionDirection::Left => Vec2::new(1.0, 0.0),
+            TransitionDirection::Right => Vec2::new(-1.0, 0.0),
+        }
+    }
+}
+
+pub struct PushTransition {
+    start_instant: Option<Instant>,
+    direction: TransitionDirection,
+    duration: Duration,
+    targets: Option<(RenderTarget, RenderTarget)>,
+    camera: OrthoCamera,
+}
+
+impl PushTransition {
+    /// * direction - The direction to enter from
+    pub fn new(game_io: &GameIO, direction: TransitionDirection, duration: Duration) -> Self {
+        let mut camera = OrthoCamera::new(game_io, Vec2::ONE);
+        camera.invert_y(true);
+
+        Self {
+            start_instant: None,
+            direction,
+            duration,
+            targets: None,
+            camera,
+        }
+    }
+}
+
+impl Transition for PushTransition {
+    fn draw(
+        &mut self,
+        game_io: &mut GameIO,
+        render_pass: &mut RenderPass,
+        draw_previous_scene: &mut dyn FnMut(&mut GameIO, &mut RenderPass),
+        draw_next_scene: &mut dyn FnMut(&mut GameIO, &mut RenderPass),
+    ) {
+        let target_size = render_pass.target_size();
+
+        // render scenes
+        let current_size = self.targets.as_ref().map(|(a, _)| a.size());
+
+        if current_size != Some(target_size) {
+            self.targets = Some((
+                RenderTarget::new(game_io, target_size),
+                RenderTarget::new(game_io, target_size),
+            ));
+        }
+
+        let (target_a, target_b) = self.targets.as_ref().unwrap();
+
+        let mut subpass = render_pass.create_subpass(target_a);
+        draw_previous_scene(game_io, &mut subpass);
+        subpass.flush();
+
+        let mut subpass = render_pass.create_subpass(target_b);
+        draw_next_scene(game_io, &mut subpass);
+        subpass.flush();
+
+        // calculate camera offset
+        let start_instant = self
+            .start_instant
+            .get_or_insert_with(|| game_io.frame_start_instant());
+
+        let mut progress = start_instant.elapsed().as_secs_f32() / self.duration.as_secs_f32();
+        progress = progress.clamp(0.0, 1.0);
+
+        let b_offset: Vec2 = self.direction.chebyshev_vector();
+        let vec = b_offset * progress;
+        self.camera
+            .set_position(Vec3::new(vec.x + 0.5, vec.y + 0.5, 0.0));
+
+        // render transition
+        let default_sprite_pipeline = game_io.resource::<DefaultSpritePipeline>().unwrap();
+        let render_pipeline = default_sprite_pipeline.as_sprite_pipeline();
+
+        let mut sprite_queue =
+            SpriteQueue::new(game_io, render_pipeline, [self.camera.as_binding()])
+                .with_inverted_y(true);
+
+        let mut sprite_a = Sprite::new(game_io, target_a.texture().clone());
+        sprite_a.set_size(Vec2::ONE);
+        sprite_queue.draw_sprite(&sprite_a);
+
+        let mut sprite_b = Sprite::new(game_io, target_b.texture().clone());
+        sprite_b.set_size(Vec2::ONE);
+        sprite_b.set_position(b_offset);
+        sprite_queue.draw_sprite(&sprite_b);
+
+        render_pass.consume_queue(sprite_queue);
+    }
+
+    fn is_complete(&self) -> bool {
+        self.start_instant
+            .map(|instant| instant.elapsed() >= self.duration)
+            .unwrap_or_default()
+    }
+}
