79d6acd79167562b723893b7442893f4cfc2d5f0
Arthur Cosentino <arthurcose@gmail.com>
2023-07-25 02:49:48 -0400

    Update file names to match structs

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,11 +1,11 @@
-mod controller;
-mod events;
+mod game_controller;
+mod input_event;
 mod input_manager;
 mod key;
 mod mouse;
 
-pub use controller::*;
-pub(crate) use events::*;
+pub use game_controller::*;
+pub(crate) use input_event::*;
 pub use input_manager::*;
 pub use key::*;
 pub use mouse::*;
